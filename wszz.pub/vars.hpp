9b0ba1cbc06e5ca476efabbbf28a8b3dd4a72d5a
y <60532776+byyyxd@users.noreply.github.com>
2021-04-26 18:07:37 -0400

    Update vars.hpp

--- wszz.pub/vars.hpp
+++ wszz.pub/vars.hpp
@@ -7,7 +7,4 @@ namespace vars
 	inline std::string pc_username;
 	inline std::string cheat_name;
 	inline std::string cheat_path;
-	inline std::string delete_folder;
-
-	inline bool deletefolder = false;
 }
